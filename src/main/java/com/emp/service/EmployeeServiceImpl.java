7b6c3d1abafa14950f9e9713d658c221c9cefb01
unknown <cogjava3100@iiht.tech>
2022-08-17 13:00:32 +0530

    first commit

---
+++ src/main/java/com/emp/service/EmployeeServiceImpl.java
@@ -0,0 +1,53 @@
+package com.emp.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.emp.exception.ResourceNotFoundException;
+import com.emp.model.Employee;
+
+@Service
+public class EmployeeServiceImpl implements IEmployeeService {
+	
+	@Autowired
+	IEmployeeRepository employeeRepository;
+	
+	@Override
+	public Integer saveEmployee(Employee employee) {
+		Employee savedEmployee = employeeRepository.save(employee);
+		return savedEmployee.getId();
+	}
+	
+	 @Override
+	 public List<Employee> getAllEmployees() {
+		 return employeeRepository.findAll();
+	 }
+        
+	 @Override
+	  public Optional<Employee> getEmployee(Integer id) {
+		 return employeeRepository.findById(id);
+	 }
+
+	@Override
+	public void deleteEmployee(Integer id) {
+		employeeRepository.deleteById(id);
+		
+	}
+
+	@Override
+	public Employee updateEmployee(Employee employee, Integer id) {
+		 //we will check weather the employee exists with the given id or not
+		Employee existingEmployee = employeeRepository.findById(id).orElseThrow(
+				()-> new ResourceNotFoundException("Employee", "id", id));
+		existingEmployee.setFirstName(employee.getFirstName());
+		existingEmployee.setFirstName(employee.getLastName());
+		existingEmployee.setFirstName(employee.getEmail());
+		
+		employeeRepository.save(existingEmployee);
+		return existingEmployee;
+		
+	}
+}
---
+++ src/main/java/com/emp/service/IEmployeeService.java
@@ -0,0 +1,19 @@
+package com.emp.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.emp.model.Employee;
+
+public interface IEmployeeService { 
+	
+	Integer saveEmployee(Employee employee);
+	
+	public List<Employee> getAllEmployees();
+
+	Optional<Employee> getEmployee(Integer id);
+	
+	public void deleteEmployee(Integer id); 
+	
+	Employee updateEmployee(Employee employee, Integer id);
+}
